50d0d55ef5d5a12e5d43064f17e11ae64e8be628
SanjayBharadwaj18 <110727875+SanjayBharadwaj18@users.noreply.github.com>
2022-08-07 16:39:00 +0530

    Create 2bproject.py

---
+++ 2bproject.py
@@ -0,0 +1,11 @@
+list1 = [11, -21, 0, 45, 66, -21]
+for num in list1:
+    
+    
+    if num >= 0:
+     print(num, end = " ")
+
+list2 =[24,-5,54,-65,12]
+for n in list2:
+  if n>=0:
+     print (n, end=" ")
