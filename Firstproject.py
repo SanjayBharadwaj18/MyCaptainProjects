3bdadd8dc861131ea6eb8e8806f71d60c4be4599
SanjayBharadwaj18 <110727875+SanjayBharadwaj18@users.noreply.github.com>
2022-08-07 15:05:48 +0530

    Update and rename README.md to Firstproject.py

---
+++ Firstproject.py
@@ -0,0 +1,14 @@
+# MyCaptainProjects Area of circle
+
+radius =float(input("enter the radius of circle "))
+print(radius)
+print("area of circle is")
+area=(22/7*radius*radius)
+print(area)
+
+
+
+#extraxting file extension
+filename= input("Enter Filename: ")
+f = filename.split(".")
+print ("Extension of the file is : ." + f[-1])
